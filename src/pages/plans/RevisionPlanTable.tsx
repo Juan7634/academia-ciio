5d94d49493c9699db489d806bc59f0c6cba756f5
Juan7633 <pedro.dominguez@bugarinlive.com>
2025-12-12 15:50:31 -0600

    Add react query

--- src/pages/plans/RevisionPlanTable.tsx
+++ src/pages/plans/RevisionPlanTable.tsx
@@ -1,5 +1,15 @@
+import { useQuery } from "@tanstack/react-query"
 
 export const RevisionPlanTable = () => {
+
+
+  const { } = useQuery({
+    queryKey: ['revision-plans'],
+  });
+
+  
+
+
   return (
     <div>RevisionPlanTable</div>
   )
